7e4f8276013475a9adbefb169a4f4e64532bbfd7
jgarsol <user@example.com>
2026-07-23 11:49:23 +0100

    Agregar configuración de devToolbar deshabilitada en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    /*devToolbar herramientas de la barra */
+    devToolbar: { 
+        enabled: false,
+    
+    }
+});
