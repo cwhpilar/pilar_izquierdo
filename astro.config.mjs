b6e3c79fc29eec6d8627373dbafabca834c8315e
jgarsol <user@example.com>
2026-07-23 11:57:33 +0100

    Actualizar comentario para clarificar la función de devToolbar en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-    /*devToolbar herramientas de la barra */
+    /*devToolbar herramientas de la barra de desarrollador */
     devToolbar: { 
         // cerramos el menu
         enabled: false,
