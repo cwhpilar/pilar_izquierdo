50273fdf46edd54dda7b7cd04b30b1c5d9f733de
jgarsol <user@example.com>
2026-07-23 13:45:37 +0100

    Actualizar configuración de devToolbar y mejorar la estructura del layout con comentarios descriptivos

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,27 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 
     /*devToolbar herramientas de la barra de desarrollador */
-    devToolbar: { 
+    devToolbar: {
         // cerramos el menu
         enabled: false,
+    },
 
-    
+    /* definimos para la internacionalización i18n */
+    i18n: {
+        //idioma por defecto en español
+        defaultLocale: 'es',
+
+        //definimos locales los directorios que hemos creado
+        locales: ['es', 'en'],
+
+        //busca las rutas
+        routing: {
+            // prefijo boleano true
+            prefixDefaultLocale: true,
+            // redireccionamiento  boleano true porque false
+            redirectToDefaultLocale: true,
+        }
     }
+
+
 });
